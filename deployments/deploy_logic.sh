38a5622e6abbd3d06564e522345abd252e43e065
maheshig99x <maheshig@99x.io>
2022-01-16 06:57:40 +0530

    fix: deployement script

--- deployments/deploy_logic.sh
+++ deployments/deploy_logic.sh
@@ -6,11 +6,11 @@ echo "Green is pointed to"
 echo $GREEN_POINT_APP
 
 if [[ $GREEN_POINT_APP == "coinbase-green" ]];then
+  kubectl delete -f deployments/blue_deployment.yaml
   kubectl apply -f deployments/blue_deployment.yaml
-  kubectl rollout restart -f deployments/blue_deployment.yaml
   echo "Deployed in Blue pods"
 else
+  kubectl delete -f deployments/green_deployment.yaml
   kubectl apply -f deployments/green_deployment.yaml
-  kubectl rollout restart -f deployments/green_deployment.yaml
   echo "Deployed in Green pods"
 fi
